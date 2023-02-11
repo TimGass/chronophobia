d7007150ca5c00bc1aca3a70fdd5e585b4664e88
timgass <timothyagass@gmail.com>
2023-02-10 19:37:58 -0600

    Added pictures and made everything work

--- server/common/server.js
+++ server/common/server.js
@@ -11,6 +11,7 @@ import rateLimit from 'express-rate-limit';
 import swaggerify from './swagger/index.js';
 import l from './logger.js';
 import { fileURLToPath } from 'url';
+import fs from "fs";
 
 const app = new Express();
 
@@ -24,15 +25,7 @@ export default class ExpressServer {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(cookieParser(process.env.SESSION_SECRET));
     app.use(Express.static(`${root}/public`));
-    app.enable("trust proxy"); // because we are behind reverse proxy (Heroku)
-    const limiter = rateLimit({
-      store: new RedisStore({
-        client: Redis.createClient(process.env.REDISCLOUD_URL || 'redis://127.0.0.1:6379', { no_ready_check: true }),
-      }),
-      windowMs: 1 * 60 * 1000, // 1 minute
-      max: 20 // limit each IP to 20 requests per minute
-    });
-    app.use(limiter);
+    app.enable("trust proxy"); // because we are behind reverse proxy (cyclic)
   }
 
   router(routes) {
@@ -40,9 +33,19 @@ export default class ExpressServer {
     return this;
   }
 
-  listen(port = process.env.PORT || 3000) {
+  async listen(port = process.env.PORT || 3000) {
+    const client = Redis.createClient({ url: process.env.REDISCLOUD_URL || 'redis://127.0.0.1:6379' });
+    await client.connect();
+    const limiter = rateLimit({
+      store: new RedisStore({
+        sendCommand: (...args) => client.sendCommand(args),
+      }),
+      windowMs: 1 * 60 * 1000, // 1 minute
+      max: 20 // limit each IP to 20 requests per minute
+    });
+    app.use(limiter);
     mongoose.set("strictQuery", false);
-    mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.7xrgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.7xrgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
     const welcome = p => () => l.info(`up and running in ${process.env.NODE_ENV || 'development'} @: ${os.hostname()} on port: ${p}}`);
     http.createServer(app).listen(port, welcome(port));
     return app;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ import './common/env.js';
 import Server from './common/server.js';
 import routes from './routes.js';
 
-export default new Server()
-  .router(routes)
-  .listen(process.env.PORT);
+const newServer = await new Server().router(routes).listen(process.env.PORT);
+
+export default newServer;
